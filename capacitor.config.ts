7c140c35f5002f6f22e70bacc3d2c0666d7605c9
Pablo-Miguel <pablomigueldelcastillo@gmail.com>
2023-02-20 18:33:12 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'PMDMO-Practica_Examen_Ionic',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
